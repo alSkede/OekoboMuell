340ebfe7cf4a322c4d42cd73d5384c5afbe7c3e9
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-07 15:27:06 +0200

    Update Scene12.jsx

--- src/components/scenes/Scene12.jsx
+++ src/components/scenes/Scene12.jsx
@@ -1,7 +1,7 @@
 import lessons from "../../data/lessons";
 import GenericSceneLayout from "../GenericSceneLayout";
 
-export default function Scene11() {
-  const scene = lessons.find(s => s.id === 11);
+export default function Scene12() {
+  const scene = lessons.find(s => s.id === 12);
   return <GenericSceneLayout scene={scene} />;
 }
