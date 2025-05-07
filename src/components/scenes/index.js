fb14ddf59f1a9dad49f06d5c1331022184b1a9da
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-07 14:53:21 +0200

    Create index.js

---
+++ src/components/scenes/index.js
@@ -0,0 +1,43 @@
+import Scene1 from "./Scene1";
+import Scene2 from "./Scene2";
+import Scene3 from "./Scene3";
+import Scene4 from "./Scene4";
+import Scene5 from "./Scene5";
+import Scene6 from "./Scene6";
+import Scene7 from "./Scene7";
+import Scene8 from "./Scene8";
+import Scene9 from "./Scene9";
+import Scene10 from "./Scene10";
+import Scene11 from "./Scene11";
+import Scene12 from "./Scene12";
+import Scene13 from "./Scene13";
+import Scene14 from "./Scene14";
+import Scene15 from "./Scene15";
+import Scene16 from "./Scene16";
+import Scene17 from "./Scene17";
+import Scene18 from "./Scene18";
+import Scene19 from "./Scene19";
+import Scene20 from "./Scene20";
+
+export default {
+  Scene1,
+  Scene2,
+  Scene3,
+  Scene4,
+  Scene5,
+  Scene6,
+  Scene7,
+  Scene8,
+  Scene9,
+  Scene10,
+  Scene11,
+  Scene12,
+  Scene13,
+  Scene14,
+  Scene15,
+  Scene16,
+  Scene17,
+  Scene18,
+  Scene19,
+  Scene20,
+};
