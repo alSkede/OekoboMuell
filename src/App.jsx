45629b614a60d73e61d22893d141d30cc95c276f
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-07 17:38:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import SceneViewer from "./components/SceneViewer";
+import CertificatePage from "./components/CertificatePage";
+<Route path="/zertifikat" element={<CertificatePage />} />
 
 function App() {
   return <SceneViewer />;
