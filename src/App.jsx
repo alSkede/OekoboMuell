a227177dedb97c1d1ec68de4bb55cfb6a8b3ac3c
alSkede <147340446+alSkede@users.noreply.github.com>
2025-05-12 13:35:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,3 @@ function App() {
 
 export default App;
 
-import Scene2 from "./components/scenes/Scene2";
-
-function App() {
-  return (
-    <div className="min-h-screen bg-orange-50 flex items-center justify-center p-6">
-      <Scene2 />
-    </div>
-  );
-}
-
-export default App;
-
