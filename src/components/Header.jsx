f1dcee56ae7bef48835c29ab5807ecf208714b73
alSkede <147340446+alSkede@users.noreply.github.com>
2025-07-06 09:35:07 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-navigation
+export default function Header() {
+  return (
+    <header className="bg-yellow-100 py-4 shadow-md">
+      <div className="flex justify-center">
+       <h1 style={{ textAlign: "center" }}>
+  😛 Wer wird Müllionär? 😛
+</h1>
+
+      </div>
+    </header>
+  );
+}
+
