ee63b28bba934cb83f4cbf8acf726d20fc755ea5
alSkede <147340446+alSkede@users.noreply.github.com>
2025-07-06 09:27:18 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StartPage from "./components/StartPage";
+import SceneViewer from "./components/SceneViewer";
+import CertificatePage from "./components/CertificatePage";
+import Header from "./components/Header";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/scenes" element={<SceneViewer />} />
+        <Route path="/zertifikat" element={<CertificatePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
