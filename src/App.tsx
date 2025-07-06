67948852e6f3b6568a39355b7ff248800d3c66b7
alSkede <147340446+alSkede@users.noreply.github.com>
2025-07-06 09:25:02 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QuizProvider } from "./components/QuizContext";
 import StartPage from "./components/StartPage";
 import SceneViewer from "./components/SceneViewer";
 import CertificatePage from "./components/CertificatePage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <QuizProvider>
-        <Routes>
-          <Route path="/" element={<StartPage />} />
-          <Route path="/szenen" element={<SceneViewer />} />
-          <Route path="/zertifikat" element={<CertificatePage />} />
-        </Routes>
-      </QuizProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/scenes" element={<SceneViewer />} />
+        <Route path="/zertifikat" element={<CertificatePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
